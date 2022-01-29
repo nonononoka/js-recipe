b34b81cb7bc95865cb4e6062529a233f7ace83eb
nonononoka <onon.kaori@gmail.com>
2022-01-29 17:17:19 +0900

    選択肢の数とボタンの表示を学習しました。

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,9 +1,6 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
-const choice4 = document.getElementById("choice-4")
+const choice = document.getElementById("choice")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
@@ -39,33 +36,47 @@ const reloadQuiz = function () {
   quizImage.src = "./images/" + quiz.image
 
   // 選択肢（ボタン）の中身を表示
+  const choice1 = document.createElement("button")
+  const choice2 = document.createElement("button")
+  const choice3 = document.createElement("button")
+  const choice4 = document.createElement("button")
+
+  choice1.className = "choice1"
+  choice2.className = "choice2"
+  choice3.className = "choice3"
+  choice4.className = "choice4"
+
   choice1.textContent = quiz.choices[0].text
   choice2.textContent = quiz.choices[1].text
   choice3.textContent = quiz.choices[2].text
   choice4.textContent = quiz.choices[3].text
-}
 
-// choiceNumber番目の選択肢を選択
-const choose = function (choiceNumber) {
-  // フィードバックを表示
-  feedback.textContent = quiz.choices[choiceNumber].feedback
-}
+  choice.append(choice1)
+  choice.append(choice2)
+  choice.append(choice3)
+  choice.append(choice4)
+  // choiceNumber番目の選択肢を選択
+  const choose = function (choiceNumber) {
+    // フィードバックを表示
+    feedback.textContent = quiz.choices[choiceNumber].feedback
+  }
 
-choice1.onclick = function () {
-  // 0 番目の選択肢を選択
-  choose(0)
-}
-choice2.onclick = function () {
-  // 1 番目の選択肢を選択
-  choose(1)
-}
-choice3.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(2)
-}
-choice4.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(3)
+  choice1.onclick = function () {
+    // 0 番目の選択肢を選択
+    choose(0)
+  }
+  choice2.onclick = function () {
+    // 1 番目の選択肢を選択
+    choose(1)
+  }
+  choice3.onclick = function () {
+    // 2 番目の選択肢を選択
+    choose(2)
+  }
+  choice4.onclick = function () {
+    // 2 番目の選択肢を選択
+    choose(3)
+  }
 }
 
 // reloadQuiz関数 を実行して、クイズを画面に表示する
