d479124e1909b2902d808d28c5e27a33164f30c5
nonononoka <onon.kaori@gmail.com>
2022-01-29 14:51:16 +0900

    発展電卓を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,20 +1,37 @@
 const display = document.getElementById("display")
 const minusButton = document.getElementById("minus-button")
-const plus2Button = document.getElementById("plus2-button")
+const plusButton = document.getElementById("plus-button")
+const kakeruButton = document.getElementById("kakeru-button")
+const waruButton = document.getElementById("waru-button")
+const num1Input = document.getElementById("num1-input")
+const num2Input = document.getElementById("num2-input")
 
 let count = 0
 
 // ボタン要素のonclickハンドラに関数を代入する
 minusButton.onclick = function () {
   // count を更新
-  count -= 1
+  count = num1Input.value - num2Input.value
   // count を表示
+  console.log(count)
   display.textContent = count
 }
 
-plus2Button.onclick = function () {
+plusButton.onclick = function () {
   // count を更新
-  count += 2
+  count = num1Input.value + num2Input.value
+  // count を表示
+  display.textContent = count
+}
+kakeruButton.onclick = function () {
+  // count を更新
+  count = num1Input.value * num2Input.value
+  // count を表示
+  display.textContent = count
+}
+waruButton.onclick = function () {
+  // count を更新
+  count = num1Input.value / num2Input.value
   // count を表示
   display.textContent = count
 }
