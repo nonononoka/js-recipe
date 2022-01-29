8d5cf76ca82d0fdf6f4b746e2d17bfd9fc42d381
nonononoka <onon.kaori@gmail.com>
2022-01-29 13:54:53 +0900

    発展3が大好きを学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,12 +1,10 @@
 const genkiFunction = function (number) {
   // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
   for (let n = 1; n <= number; n++) {
-    if (n % 15 === 0) {
-      console.log("FizzBuzz")
-    } else if (n % 3 === 0) {
-      console.log("Fizz")
-    } else if (n % 5 === 0) {
-      console.log("Buzz")
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!`)
+    } else if (String(n).includes("3")) {
+      console.log(`${n}!!!!!`)
     } else {
       console.log(n)
     }
