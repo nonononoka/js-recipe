aa2dcb21ce879a6e617a8d4a5f8711cd9003af94
nonononoka <onon.kaori@gmail.com>
2022-01-29 14:21:44 +0900

    発展便利なボタンを学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,12 +1,20 @@
 const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const plus2Button = document.getElementById("plus2-button")
 
 let count = 0
 
 // ボタン要素のonclickハンドラに関数を代入する
-plusButton.onclick = function () {
+minusButton.onclick = function () {
   // count を更新
-  count += 1
+  count -= 1
+  // count を表示
+  display.textContent = count
+}
+
+plus2Button.onclick = function () {
+  // count を更新
+  count += 2
   // count を表示
   display.textContent = count
 }
