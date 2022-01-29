fe9136ecf5f46d3477a12e12376b6d5ca2facf87
nonononoka <onon.kaori@gmail.com>
2022-01-29 15:10:02 +0900

    発展いろんなイベントを学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,10 @@
 const figure = document.getElementById("figure")
+const Button = document.getElementById("button")
 
 figure.onclick = function () {
+  figure.classList.toggle("rounded")
+}
+
+Button.onclick = function () {
   figure.classList.toggle("rounded")
 }
